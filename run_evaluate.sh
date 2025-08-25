6e43e148cc98b548a387a95b4ffb771d558760dc
liygong <liygong@octane-ord-non-restricted-vm-cpu-ad1-001.compute.nrphxvcnc.oraclevcn.com>
2025-08-25 04:21:00 +0000

    add sample_n to evaluate.py

--- run_evaluate.sh
+++ run_evaluate.sh
@@ -6,16 +6,19 @@ mkdir -p results_block
 python evaluate.py \
   block \
   outputs_block/${MDL}-fim-tb.jsonl \
-  results_block/${MDL}-fim-tb.jsonl
+  results_block/${MDL}-fim-tb.jsonl \
+  --sample_n 20
 
 mkdir -p results_control
 python evaluate.py \
   control \
   outputs_control/${MDL}-fim-tc.jsonl \
-  results_control/${MDL}-fim-tc.jsonl
+  results_control/${MDL}-fim-tc.jsonl \
+  --sample_n 20
 
 mkdir -p results_api
 python evaluate.py \
   api \
   outputs_api/${MDL}-fim-ta.jsonl \
-  results_api/${MDL}-fim-ta.jsonl
+  results_api/${MDL}-fim-ta.jsonl \
+  --sample_n 20
